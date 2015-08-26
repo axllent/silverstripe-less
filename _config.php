b119c0c542e40a14e4bd8a0e5607c803eabb2eff
vizo <vizo@users.noreply.github.com>
2015-08-27 00:32:58 +0200

    Added caching options

--- _config.php
+++ _config.php
@@ -3,3 +3,9 @@ Requirements::set_backend(new LessCompiler());
 
 /* Add default ThemeDir variable */
 LessCompiler::addVariable('ThemeDir', '"' . Director::baseURL() . SSViewer::get_theme_folder() . '"');
+
+/* Set default cache directory */
+LessCompiler::setCacheDir(Director::baseFolder() . '/' . SSViewer::get_theme_folder() . '/.tmp/less');
+
+/* Set default cache method */
+LessCompiler::setCacheMethod('serialize');
